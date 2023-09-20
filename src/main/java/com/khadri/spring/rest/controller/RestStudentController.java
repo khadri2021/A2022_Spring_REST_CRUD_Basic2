20d1841a4620d838dd90cbfbc401f16c83f4427e
khadri2021 <khadri.institutions@gmail.com>
2023-09-20 10:15:02 +0530

    Basic CURD operations with HashMap

---
+++ src/main/java/com/khadri/spring/rest/controller/RestStudentController.java
@@ -0,0 +1,56 @@
+package com.khadri.spring.rest.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestStudentController {
+
+	private static HashMap<Integer, String> hashMap = new HashMap<>();
+
+	@PostMapping("/register/add")
+	public String registerStudent(Integer id, String name) {
+
+		hashMap.put(id, name);
+
+		return id + " Student Added";
+	}
+
+	@GetMapping(value = "/register/view")
+	public HashMap<Integer, String> readAllRegisteredStudents() {
+
+		return hashMap;
+	}
+
+	@PutMapping("/register/update")
+	public HashMap<Integer, String> registerUpdateStudent(Integer id, String name) {
+
+		if (hashMap.containsKey(id)) {
+			hashMap.put(id, name);
+		} else {
+			hashMap.put(id, name);
+		}
+
+		return hashMap;
+	}
+
+	@DeleteMapping("/register/delete")
+	public HashMap<Integer, String> registerDeleteStudent(Integer id) {
+
+		hashMap.remove(id);
+
+		return hashMap;
+	}
+
+}
